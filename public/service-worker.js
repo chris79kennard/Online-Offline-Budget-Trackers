9eda2c773bf0d5f9530f5b472edb914041999c27
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 20:08:17 -0600

    removed excess files

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,14 +3,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/assets/css/style.css",
   "/manifest.webmanifest",
-  "/favicon.ico",
-  "/assets/images/icons/icon-72x72.png",
-  "/assets/images/icons/icon-96x96.png",
-  "/assets/images/icons/icon-128x128.png",
-  "/assets/images/icons/icon-144x144.png",
-  "/assets/images/icons/icon-152x152.png",
   "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-384x384.png",
   "/assets/images/icons/icon-512x512.png",
 ];
 
